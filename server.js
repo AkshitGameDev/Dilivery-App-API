71244a9f29c5130343dc365e8a24ffddb57005cd
Akshit Sharma <akshitsharma196@gmail.com>
2025-09-25 18:04:15 -0400

    fetching

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
+// server.js
 require('dotenv').config();
 const app = require('./app');
-const { redis } = require('./lib/redis');
 const port = process.env.PORT || 4000;
 const server = app.listen(port, () => console.log(`HTTP on :${port}`));
-['SIGINT','SIGTERM'].forEach(s=>process.on(s, async ()=>{ try{await redis.quit();}catch{} server.close(()=>process.exit(0)); }));
